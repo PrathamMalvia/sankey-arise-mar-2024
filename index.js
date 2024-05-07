5f226f7930a0df871d17266ffb8a38bf75f875b2
PrathamMalvia <malviapratham1@gmail.com>
2024-05-07 14:53:34 +0530

    add mongodb connection

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
+// Libraries
 import express from "express";
+import 'dotenv/config';
+import mongoose from "mongoose";
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.send("Hello");
 });
 
-app.listen(PORT, (req, res) => {
-    console.log("Server is running");
-})
+
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => {
+        console.log("App connected to database");
+        app.listen(PORT, (req, res) => {
+            console.log("Server is running");
+        })
+    })
+    .catch((error) => {
+        console.log(error);
+    })
